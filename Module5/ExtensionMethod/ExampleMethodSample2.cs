ed6f49cbecbd55d1f94dc05e24383889173a708c
jhong6 <54685172+jhong6@users.noreply.github.com>
2019-11-05 10:34:04 -0800

    Create ExampleMethodSample2.cs

---
+++ Module5/ExtensionMethod/ExampleMethodSample2.cs
@@ -0,0 +1,24 @@
+using System;
+namespace ExtensionMethod
+{
+    class Geek
+    {
+        //Method 1
+        public void M1()
+        {
+            Console.WriteLine("Method Name: M1");
+        }
+
+        //Method 2
+        public void M2()
+        {
+            Console.WriteLine("Method Name: M2");
+        }
+
+        //Method 3
+        public void M3()
+        {
+            Console.WriteLine("Method Name: M3");
+        }
+    }
+}
