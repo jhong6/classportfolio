3320f54f2fc076bfdbd8b70c4efabeb8d0e2db59
jhong6 <54685172+jhong6@users.noreply.github.com>
2019-11-05 10:33:05 -0800

    Create ExtensionMethodSample1.cs

---
+++ Module5/ExtensionMethod/ExtensionMethodSample1.cs
@@ -0,0 +1,29 @@
+using System;
+namespace ExtensionMethod
+{
+    static class NewMethodClass
+    {
+        //Method 4
+        public static void M4(this Geek g)
+        {
+            Console.WriteLine("Method Name: M4");
+        }
+        //Method 5
+        public static void M5(this Geek g, string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Geek g = new Geek();
+            g.M1();
+            g.M2();
+            g.M3();
+            g.M4();
+            g.M5("Method Name: M5");
+        }
+    }
+}
