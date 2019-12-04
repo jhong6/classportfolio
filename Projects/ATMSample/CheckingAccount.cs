f0a8bca3ff773c3f15c4ee8d78d19e380dcc3881
jhong6 <54685172+jhong6@users.noreply.github.com>
2019-12-03 19:36:56 -0800

    Create CheckingAccount.cs

---
+++ Projects/ATMSample/CheckingAccount.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATMforCsharp
+{
+    class CheckingAccount
+    {
+        public int count;
+        public double accountTotal;
+        public double extraCharge;
+
+        public CheckingAccount(double accountBalance)
+        {
+            accountTotal = accountBalance;
+        }
+        void withdrawl(double total)
+        {
+            if(count >= 4)
+            {
+                extraCharge = 1.5;
+                total += extraCharge;
+            }
+            if(this.accountTotal >= total)
+            {
+                this.accountTotal -= total;
+                count++;
+            }
+            else
+            {
+                double requiredAmount = total - this.accountTotal;
+                throw new InsufficientFund(requiredAmount);
+            }
+        }
+        // Method for deposit
+        void deposit(double total)
+        {
+            //Adds to the current amount in
+            this.accountTotal += total;
+        }
+
+        void transfer(CheckingAccount withdrawAcc, SavingsAccount savingsAcc, double total)
+        {
+            if(withdrawAcc.accountTotal >= total)
+            {
+                withdrawAcc.accountTotal -= total;
+                savingsAcc.accountSTotal += total;
+            }
+        }
+
+        // Method to return Balance
+        double balance()
+        {
+            return accountTotal;
+        }
+
+    }
+}
