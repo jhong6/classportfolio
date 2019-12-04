1fae31c2fb338ba4d91fe135ecdb5a2ea930c67c
jhong6 <54685172+jhong6@users.noreply.github.com>
2019-12-03 19:36:30 -0800

    Create SavingsAccount.cs

---
+++ Projects/ATMSample/SavingsAccount.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATMforCsharp
+{
+    class SavingsAccount
+    {
+        public double accountSTotal;
+        public double bonusAmount;
+
+        public SavingsAccount (double accountsavings)
+        {
+            this.accountSTotal = accountsavings;
+        }
+        void savingswithdrawl(double total)
+        {
+            if (this.accountSTotal >= total)
+            {
+                this.accountSTotal -= total;
+            }
+            else
+            {
+                double requiredAmount = total - this.accountSTotal;
+                throw new InsufficientFund(requiredAmount);
+            }
+        }
+        void savingsdeposit(double total)
+        {
+            //Adds to the current amount in
+            this.accountSTotal += total;
+        }
+        void transfer(CheckingAccount withdrawAcc, SavingsAccount savingsAcc, double total)
+        {
+            if (withdrawAcc.accountTotal >= total)
+            {
+                withdrawAcc.accountTotal -= total;
+                savingsAcc.accountSTotal += total;
+            }
+        }
+        double balance()
+        {
+            return accountTotal;
+        }
+    }
+}
