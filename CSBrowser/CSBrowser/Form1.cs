1adfe95e0ffeeefb38cd24d6faf67887a48f3bb0
jhong6 <54685172+jhong6@users.noreply.github.com>
2019-12-09 16:17:50 -0800

    Update Form1.cs

--- CSBrowser/CSBrowser/Form1.cs
+++ CSBrowser/CSBrowser/Form1.cs
@@ -19,37 +19,50 @@ namespace CSBrowser
 
         private void Button2_Click(object sender, EventArgs e)
         {
+            // Check to see if the button can go back to the
+            // previous page
             if(webBrowser1.CanGoBack)
             {
+                // If the condition is met, call the GoBack()
                 webBrowser1.GoBack();
             }
         }
 
         private void Button3_Click(object sender, EventArgs e)
         {
+            // Like the Back button, check if going forward is
+            // allowed.
             if(webBrowser1.CanGoForward)
             {
+                // Call the .GoForward if condition is met
                 webBrowser1.GoForward();
             }
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
+            // Calls the GoHome(), which calls the url for Home
+            // page
             webBrowser1.GoHome();
         }
 
         private void Button4_Click(object sender, EventArgs e)
         {
+            // Calls the Refresh to allow reloading of the page
             webBrowser1.Refresh();
         }
 
         private void Button6_Click(object sender, EventArgs e)
         {
+            // Stops the web browser while loading the page.
             webBrowser1.Stop();
         }
 
         private void Button5_Click(object sender, EventArgs e)
         {
+            // Set string variable to the contents(the url) that
+            // removes extra spaces from both the beginning and
+            // end (if there are any)
             string WebPage = textBox1.Text.Trim();
             webBrowser1.Navigate(WebPage);
         }
