88421535cf5ac822aff3a13c76a510cc2dffc909
jhong6 <54685172+jhong6@users.noreply.github.com>
2019-12-10 22:05:02 -0800

    Update Program2.cs

--- ExtensionMethods/Program2.cs
+++ ExtensionMethods/Program2.cs
@@ -22,17 +22,5 @@ namespace ExtensionMethod
             Console.WriteLine(str);
         }
     }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Geek g = new Geek();
-            g.M1();
-            g.M2();
-            g.M3();
-            g.M4();
-            g.M5("Method Name: M5");
-        }
-    }
 }
 
