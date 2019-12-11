a808169825bb15879c3d020c7da5c24c1fc5b70f
jhong6 <54685172+jhong6@users.noreply.github.com>
2019-12-10 22:10:15 -0800

    Update Program.cs

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -6,13 +6,16 @@
 //           exisiting class without modifying the source code
 //           of the original type.
 
-public class GFG { 
-  
+using System;
+namespace ExtensionMethod
+{
+  public class GFG 
+  { 
     // Main Method 
     public static void Main(string[] args) 
     { 
         // Instantiate a new object called g from
-        // class Geek
+        // class Geek and has access to all methods
         Geek g = new Geek(); 
         
         // Call all the methods, including
@@ -20,6 +23,26 @@ public class GFG {
         g.M2(); 
         g.M3(); 
         g.M4(); 
-        g.M5("Method Name: M5"); 
+        g.M5("This is the 5th method"); 
     } 
-} 
+}
+
+ // Modification: Minimum. Added comments and 2
+ //               custom methods.
+  
+ // Analysis:     The program simply demonstrates
+ //               accessing different methods
+ //               by using the binding parameter
+ //               (using "this" keyword).
+ 
+ // Output:
+ // Method Name: M1
+ // Enter your name:
+ // Bob
+ // Hello Bob
+ // Enter a value for x:
+ // 3
+ // Enter a value for y:
+ // The sum of the two is: 5
+ // Method Method Name: M4
+ // This is the 5th method
