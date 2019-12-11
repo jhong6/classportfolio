542b9e6ad23a0d0adf52ee78f38833ec1f5dfac6
jhong6 <54685172+jhong6@users.noreply.github.com>
2019-12-10 21:56:35 -0800

    Update Geek.cs

--- ExtensionMethods/Geek.cs
+++ ExtensionMethods/Geek.cs
@@ -1,3 +1,11 @@
+// Source: https://www.geeksforgeeks.org/extension-method-in-c-sharp/
+// Extension Method in C#
+// Author: ankita_saini
+// Student: Joseph Hong
+// Synopsis: Extension method allows adding new methods in the
+//           exisiting class without modifying the source code
+//           of the original type.
+
 using System;
 namespace ExtensionMethod
 {
@@ -12,14 +20,20 @@ namespace ExtensionMethod
         //Method 2
         public void M2()
         {
-            Console.WriteLine("Method Name: M2");
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
         }
 
         //Method 3
         public void M3()
         {
-            Console.WriteLine("Method Name: M3");
+            int x, y;
+            Console.WriteLine("Enter a value for x:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a value for y:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The sum of the two is: " + (x+y));
         }
     }
 }
-//Source: https://www.geeksforgeeks.org/extension-method-in-c-sharp/
